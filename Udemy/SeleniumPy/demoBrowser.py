14da86e4e71e39f0effd80af117f2dd2c09dfd1f
Suresh <sureshkumar_a@outlook.com>
2020-04-07 17:27:50 +0530

    +sel packages and sele basics

--- Udemy/SeleniumPy/demoBrowser.py
+++ Udemy/SeleniumPy/demoBrowser.py
@@ -1,2 +1,11 @@
 from selenium import webdriver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("C:\\chromedriver.exe")
+#driver = webdriver.Firefox("C:\\geckodriver.exe")
+driver.get("https://www.google.com/")
+driver.maximize_window()
+print(driver.title)
+print(driver.current_url)
+driver.get("https://www.rahulshettyacademy.com/AutomationPractice")
+driver.back()
+driver.refresh()
+driver.close()
---
+++ Udemy/SeleniumPy/locators.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+driver = webdriver.Chrome("C:\\chromedriver.exe")
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+driver.find_element_by_name("name").send_keys("Suresh")
+driver.find_element_by_id("exampleInputPassword1").send_keys("password")
+driver.find_element_by_name("email").send_keys("as@d.com")
+driver.find_element_by_id("exampleCheck1").click()
+driver.find_element_by_xpath("//input[@type='submit']").click()
+
+
